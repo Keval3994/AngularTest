5764fcfed1b1f7f2350e82acb8ebe3a955558c55
Keval3994 <30352275+Keval3994@users.noreply.github.com>
2018-09-28 08:09:56 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-
+import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponentComponent } from './Component/header-component/header-component.component';
+import { FooterComponentComponent } from './Component/footer-component/footer-component.component';
+import { LeftNavComponent } from './Component/left-nav/left-nav.component';
+import { MainBodyComponent } from './Component/main-body/main-body.component';
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    HeaderComponentComponent,
+    FooterComponentComponent,
+    LeftNavComponent,
+    MainBodyComponent  ],
   imports: [
-    BrowserModule
+    BrowserModule,FormsModule, AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
